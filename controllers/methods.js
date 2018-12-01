1f18916b289a35afd4d3ffcd8e372c01ef4938fe
Ricardo <00168117@uca.edu.sv>
2018-12-01 16:27:18 -0600

    archivos js agregados para despues y prueba de añadir iconos como boton

---
+++ controllers/methods.js
@@ -0,0 +1,16 @@
+
+router.get('/api/Investigaciones', (req, res) => {
+
+});
+
+router.get('/api/Investigaciones/:id', (req, res) => {
+
+});
+
+router.put('/api/Investigaciones/:id', async (req, res) => {
+
+});
+
+router.delete('/api/Investigaciones/delete', async (req, res) => {
+
+});
---
+++ models/Inv.js
@@ -0,0 +1,2 @@
+const InvSchema = new Schema();
+
